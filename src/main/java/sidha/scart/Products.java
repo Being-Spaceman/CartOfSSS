a72a41736cd1200941e1037774822754abdc1c9a
Being-Spaceman <bhatikares@gmail.com>
2016-08-17 15:25:30 +0530

    Add files via upload

---
+++ src/main/java/sidha/scart/Products.java
@@ -0,0 +1,38 @@
+package sidha.scart;
+
+public class Products {
+	private String pName;
+	private String pDetails;
+	private int pPrice;	
+	
+	
+	
+	
+	private int pID;
+	public int getpID() {
+		return pID;
+	}
+	public void setpID(int pID) {
+		this.pID = pID;
+	}
+	public String getpName() {
+		return pName;
+	}
+	public void setpName(String pName) {
+		this.pName = pName;
+	}
+	public String getpDetails() {
+		return pDetails;
+	}
+	public void setpDetails(String pDetails) {
+		this.pDetails = pDetails;
+	}
+	public int getpPrice() {
+		return pPrice;
+	}
+	public void setpPrice(int pPrice) {
+		this.pPrice = pPrice;
+	}
+	
+
+}
